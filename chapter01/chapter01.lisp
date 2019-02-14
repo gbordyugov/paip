a797a0082eb89b7bde22e0bdc5a0a1f88f5d6a32
Grigory Bordyugov <gregbg@gmail.com>
2019-02-14 09:13:11 +0100

    Chapter 1.1 done

--- chapter01/chapter01.lisp
+++ chapter01/chapter01.lisp
@@ -1,3 +1,17 @@
+(+ 2 2)
+
+(+ 1 2 3 4 5 6 7 8 9 10)
+
+(- (+ 9000 900 90 9) (+ 5000 500 50 5))
+
+'(Pat Kim)
+
+(append '(pat kim) '(robin sandy))
+
+(append '(pat kim) (list '(john q public) 'sandy))
+
+(length (append '(pat kim) (list '(john q public) 'sandy)))
+
 (defun last-name (name)
   "Select the lasat name from a name represented as a list."
   (first (last name)))
