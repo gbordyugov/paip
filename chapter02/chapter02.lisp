2f270a3202faf721d64f031ee7af15e56c3380d3
Grigory Bordyugov <gregbg@gmail.com>
2019-02-18 10:04:59 +0100

    Chapter 2 a rule-based solution

--- chapter02/chapter02.lisp
+++ chapter02/chapter02.lisp
@@ -55,3 +55,50 @@
   (one-of '(to in by with on)))
 
 (sentence)
+
+;;
+;; 2.3 A Rule-Based Solution
+;;
+
+(defparameter *simple-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Noun))
+    (verb-phrase -> (Verb noun-phrase))
+    (Article -> the a)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked))
+  "A grammar for a trivial subset of English.")
+
+(defvar *grammar* *simple-grammar*
+  "The grammar used by generate. Initially, this is *simple-grammar*,
+   but we can switch to other grammars.")
+
+;; (assoc 'noun *grammar*)
+
+(defun rule-lhs (rule)
+  "The left-hand side of a rule."
+  (first rule))
+
+(defun rule-rhs (rule)
+  "The right-hand side of a rule."
+  (rest (rest rule)))
+
+(defun rewrites (category)
+  "Return a list of the possible rewrites for this category."
+  (rule-rhs (assoc category *grammar*)))
+
+(defun mappend (fn the-list)
+  "Apply fn to each element of list and append the results."
+  (apply #'append (mapcar fn the-list)))
+
+(defun generate (phrase)
+  (cond ((listp phrase)
+         (mappend #'generate phrase))
+        ((rewrites phrase)
+         (generate (random-elt (rewrites phrase))))
+        (t (list phrase))))
+
+
+(generate 'sentence)
+(generate 'noun-phrase)
+(generate 'verb-phrase)
