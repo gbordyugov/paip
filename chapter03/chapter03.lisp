ac6b00d11dbacb5a1f6d4729a6ad3d74e38fe96e
Grigory Bordyugov <gregbg@gmail.com>
2019-02-21 09:16:31 +0100

    Chapter 3 structures

--- chapter03/chapter03.lisp
+++ chapter03/chapter03.lisp
@@ -1,3 +1,26 @@
 ;;
 ;; Chapter 3.2 Special Forms
 ;;
+
+(defstruct name
+  first
+  (middle nil)
+  last)
+
+(setf b (make-name :first 'Barney :last 'Rubble))
+
+b
+
+(name-first b)
+
+(name-middle b)
+
+(name-last b)
+
+(name-p b)
+
+(name-p 'Barney)
+
+(setf (name-middle b) 'Q)
+
+b
