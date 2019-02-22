7be28e2521055aeb4afbf6009f83d67d3a692306
Grigory Bordyugov <gregbg@gmail.com>
2019-02-22 10:02:58 +0100

    Chapter 3.7 functions on trees done

--- chapter03/chapter03.lisp
+++ chapter03/chapter03.lisp
@@ -354,3 +354,37 @@ b
 
 (get 'AK 'state)
 (get 'TX 'state)
+
+;;
+;; 3.7 Functions on Trees
+;;
+
+(setf tree '((a b) ((c)) (d e)))
+(tree-equal tree (copy-tree tree))
+
+(defun same-shape-tree (a b)
+  "Are two trees the same except for the leaves?"
+  (tree-equal a b :test #'true))
+
+(defun true (&rest ignore) t)
+
+(same-shape-tree tree '((1 2) ((3)) (4 5)))
+(same-shape-tree tree '((1 2)  (3)  (4 5)))
+
+(subst 'new 'old '(old ((very old))))
+(sublis '((old . new)) '(old ((very old))))
+(subst 'new 'old 'old)
+
+(defun english->french (words)
+  (let ((translations
+         '((are . va)
+           (book .libre)
+           (friend . ami)
+           (hello . bonjour)
+           (how . comment)
+           (my . mon)
+           (red . rouge)
+           (you . tu))))
+    (sublis translations words)))
+
+(english->french '(hello my friend - how are you today?))
