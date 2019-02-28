6192e798943909ceec22fa29be014452dae472ee
Grigory Bordyugov <gregbg@gmail.com>
2019-02-28 09:48:08 +0100

    re-writing Exercise 3.5

--- chapter03/twenty-questions.lisp
+++ chapter03/twenty-questions.lisp
@@ -2,38 +2,15 @@
 ;; Exercise 3.5
 ;;
 
-(defun ask (thing)
-  (progn
-    (format t "Is it ~A?" thing)
-    (case (read)
-      ((y yes) (prin1 'wow!))
-      ((n no ) (prin1 'nopes))
-      (t       (prin1 'whatever)))
-    'ok))
-
-(read)
-
-(ask 'thing)
-
-'bla
-
-(defun twenty-questions ()
-  t)
-
-(defstruct question
-  name
-  (yes nil)
-  (no  nil))
-
-(make-question :name 'thing)
-
 ;;
-;; just playing around with the syntax of Common Lisp
+;; constructor of the data structure
 ;;
-
 (defun make-thing (name &optional (yes nil) (no nil))
   (list name yes no))
 
+;;
+;; accessors
+;;
 (defun thing-name (thing)
   (nth 0 thing))
 
@@ -43,20 +20,21 @@
 (defun thing-no (thing)
   (nth 2 thing))
 
-(defun update-thing-with-answer (thing answer)
-  (case answer
-    ((y yes) (let ((name      (thing-name thing))
-                   (yes-child (thing-yes thing))
-                   ( no-child (thing-no thing))
-                   (next-answer    (question)))
-               (make-thing name
-                           (update-thing-with-answer next-answer)
-                           no-child)))
-    ((n no) (let ((name      (thing-name thing))
-                  (yes-child (thing-yes thing))
-                  ( no-child (thing-no thing))
-                  (next-answer    (question)))
-              (make-thing name
-                          yes-child
-                          (update-thing-with-answer next-answer))))
-    (it thing)))
+;;
+;; the main recursion
+;;
+
+(defun rec (thing)
+  (case (ask-about thing)
+    (yes t)
+    (no  t)
+    (it  t)))
+
+(defun ask-about (thing)
+  (progn
+    (format t "Is it ~A?" (thing-name thing))
+    (case (read)
+      ((y yes) 'yes)
+      ((n no ) 'no)
+      (it      'it)
+      (t       (ask-about thing)))))
