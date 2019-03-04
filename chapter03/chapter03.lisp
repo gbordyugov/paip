5d8f159f1522d62331c3d45358314d5489090b69
Grigory Bordyugov <gregbg@gmail.com>
2019-03-04 10:03:37 +0100

    Chapter 3 math-quiz

--- chapter03/chapter03.lisp
+++ chapter03/chapter03.lisp
@@ -525,3 +525,21 @@ y
 (show-both 5.1)
 
 (values 1 2 3)
+
+;;
+;; 3.19 More about Parameters
+;;
+
+(defun math-quiz (op range n)
+  "Ask the user a series of math problems."
+  (dotimes (i n)
+    (problem (random range) op (random range))))
+
+(defun problem (x op y)
+  "Ask a math problem, read a reply, and say if it is correct."
+  (format t "~&How much is ~d ~a ~d?" x op y)
+  (if (eql (read) (funcall op x y))
+      (princ "Correct!")
+      (princ "Sorry, that's not right.")))
+
+(math-quiz '+ 5 5)
