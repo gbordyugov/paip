ab09f3f7b8a1459734f5b0c83038c28a7b4f4344
Grigory Bordyugov <gregbg@gmail.com>
2019-03-07 10:20:55 +0100

    Chapter 4: a couple more GPS calls

--- chapter04/chapter04.lisp
+++ chapter04/chapter04.lisp
@@ -99,3 +99,9 @@
 (gps '(son-at-home car-needs-battery have-money have-phone-book)
      '(son-at-school)
      *school-ops*)
+
+(gps '(son-at-home car-needs-battery have-money)
+     '(son-at-school)
+     *school-ops*)
+
+(gps '(son-at-home car-works) '(son-at-school) *school-ops*)
