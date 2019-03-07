27ac381c371e6ce435e8bbf608e4d00a117bcd81
Grigory Bordyugov <gregbg@gmail.com>
2019-03-07 10:17:43 +0100

    Chapter 4 running GPS

--- chapter04/chapter04.lisp
+++ chapter04/chapter04.lisp
@@ -67,3 +67,35 @@
              :test-not (complement test-not) keyword-args)
       (apply #'remove item sequence
             :test (complement test) keyword-args)))
+
+;;
+;; The example from the book
+;;
+
+(defparameter *school-ops*
+  (list
+   (make-op :action 'drive-son-to-school
+            :preconds '(son-at-home car-works)
+            :add-list '(son-at-school)
+            :del-list '(son-at-home))
+   (make-op :action 'shop-installs-battery
+            :preconds '(car-needs-battery shop-knows-problem
+                        shop-has-money)
+            :add-list '(car-works))
+   (make-op :action 'tell-shop-problem
+            :preconds '(in-communication-with-shop)
+            :add-list '(shop-knows-problem))
+   (make-op :action 'telephone-shop
+            :preconds '(know-phone-number)
+            :add-list '(in-communication-with-shop))
+   (make-op :action 'look-up-number
+            :preconds '(have-phone-book)
+            :add-list '(know-phone-number))
+   (make-op :action 'give-shop-money
+            :preconds '(have-money)
+            :add-list '(shop-has-money)
+            :del-list '(have-money))))
+
+(gps '(son-at-home car-needs-battery have-money have-phone-book)
+     '(son-at-school)
+     *school-ops*)
