47da591a31404ba55beeda3c1524050c0384f0c4
Grigory Bordyugov <gregbg@gmail.com>
2019-03-11 09:59:16 +0100

    working on an immutable version of the GPS solver from Chapter 4

---
+++ chapter04/gps-immutable.lisp
@@ -0,0 +1,27 @@
+;;
+;; A purely immutable version of the GPS from Chapter 4.
+;;
+
+;; An operator is represented as a structure composed of an action, a
+;; list of preconditions, and a list of effects. The list of effects
+;; can be split into an add-list (what conditions this operator adds)
+;; and a delete-list (what conditions this operator deletes).
+(defstruct op "An operation"
+           (action nil)
+           (preconds nil)
+           (add-list nil)
+           (del-list nil))
+
+
+;;
+;; Auxiliary functions.
+;;
+(defun find-all (item sequence &rest keyword-args
+                 &key (test #'eql) test-not &allow-other-keys)
+  "Find all those elements of sequence that match item,
+   according to the keywords. Doesn't alter sequence."
+  (if test-not
+      (apply #'remove item sequence
+             :test-not (complement test-not) keyword-args)
+      (apply #'remove item sequence
+            :test (complement test) keyword-args)))
