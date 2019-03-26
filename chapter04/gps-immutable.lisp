1d0fa5832f95c96673f67f7a1d19ef2692d2413b
Grigory Bordyugov <gregbg@gmail.com>
2019-03-26 09:36:11 +0100

    Chapter 4 working on an immutable GPS

--- chapter04/gps-immutable.lisp
+++ chapter04/gps-immutable.lisp
@@ -26,13 +26,22 @@
    otherwise nil."
   t)
 
-
-
 (defun achive-goal (from-state goal ops)
   "Try to achive goal from from-state using operators ops. Return
    state containing goal if goal is achievable, otherwise nil."
-  t)
+  (if (goal-achieved-p from-state goal)
+      from-state
+      ;; if goal is not part of from-state, we need to apply one of ops
+      t))
 
+(defun ops-pointing-at (ops goal)
+  (find-all goal ops :test #'(lambda (goal op)
+                               (member goal (op-add-list op)))))
+
+(ops-pointing-at ops 'son-at-school)
+
+(defun goal-achieved-p (state goal)
+  (member goal state))
 
 ;;
 ;; Auxiliary functions.
@@ -59,3 +68,27 @@
     (reverse (rec seq '()))))
 
 (filter #'oddp '(1 2 3 4 5 6 7))
+
+(setq ops
+  (list
+   (make-op :action 'drive-son-to-school
+            :preconds '(son-at-home car-works)
+            :add-list '(son-at-school)
+            :del-list '(son-at-home))
+   (make-op :action 'shop-installs-battery
+            :preconds '(car-needs-battery shop-knows-problem
+                        shop-has-money)
+            :add-list '(car-works))
+   (make-op :action 'tell-shop-problem
+            :preconds '(in-communication-with-shop)
+            :add-list '(shop-knows-problem))
+   (make-op :action 'telephone-shop
+            :preconds '(know-phone-number)
+            :add-list '(in-communication-with-shop))
+   (make-op :action 'look-up-number
+            :preconds '(have-phone-book)
+            :add-list '(know-phone-number))
+   (make-op :action 'give-shop-money
+            :preconds '(have-money)
+            :add-list '(shop-has-money)
+            :del-list '(have-money))))
