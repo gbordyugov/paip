0f8a62c318213e85929d39c65ee445f89781f2db
Grigory Bordyugov <gregbg@gmail.com>
2019-03-26 09:21:07 +0100

    fixing prev commit

--- chapter04/gps-immutable.lisp
+++ chapter04/gps-immutable.lisp
@@ -24,6 +24,7 @@
   "Try to achive to-state from from-state using operators ops. Return
    state which is (possibly) a superset of to-state if achievalbe,
    otherwise nil."
+
   t)
 
 (defun achive-goal (from-state goal ops)
