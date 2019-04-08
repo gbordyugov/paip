71ab3acaec067782d11c15dd42aa325e49d94f0f
Grigory Bordyugov <gregbg@gmail.com>
2019-04-08 09:52:12 +0200

    Chapter 4 GPS v2

--- chapter04/gps-v2.lisp
+++ chapter04/gps-v2.lisp
@@ -1,3 +1,23 @@
 ;;
 ;; Chapter 4.11 GPS Version 2: A More General Problem solver
 ;;
+
+(defun executing-p (x)
+  "Is x of the form: (executing ...)?"
+  (starts-with x 'executing))
+
+(defun starts-with (list x)
+  "Is this a list whose first element is x?"
+  (and (consp list) (eql (first list) x)))
+
+(defun convert-op (op)
+  "Make op conform to the (EXECUTING op) convention."
+  (unless (some #'executing-p (op-add-list op))
+    (push (list 'executing (op-action op)) (op-add-list op)))
+  op)
+
+(defun op (action &key preconds add-list del-list)
+  "Make a new operator that obeys the (EXECUTING op) convention."
+  (convert-op
+   (make-op :action action :preconds preconds :add-list add-list
+            :del-list del-list)))
