8b542c929e7b6c53351ea8e6b6f11711f3ea6fa0
Grigory Bordyugov <gregbg@gmail.com>
2019-04-09 10:05:22 +0200

    Chapter 4 GPS v2: shuffling code around

--- chapter04/gps-v2.lisp
+++ chapter04/gps-v2.lisp
@@ -2,30 +2,6 @@
 ;; Chapter 4.11 GPS Version 2: A More General Problem solver
 ;;
 
-(defun executing-p (x)
-  "Is x of the form: (executing ...)?"
-  (starts-with x 'executing))
-
-(defun starts-with (list x)
-  "Is this a list whose first element is x?"
-  (and (consp list) (eql (first list) x)))
-
-;; Since the elements of the add-list are to be added to the state
-;; upon execution of the operator, adding a (executing ...) form to
-;; the add-list make this (executing ...) form appear on the list
-;; which represents the state after the application of the operator.
-(defun convert-op (op)
-  "Make op conform to the (EXECUTING op) convention."
-  (unless (some #'executing-p (op-add-list op))
-    (push (list 'executing (op-action op)) (op-add-list op)))
-  op)
-
-(defun op (action &key preconds add-list del-list)
-  "Make a new operator that obeys the (EXECUTING op) convention."
-  (convert-op
-   (make-op :action action :preconds preconds :add-list add-list
-            :del-list del-list)))
-
 (defstruct op "An operation"
            (action nil)
            (preconds nil)
@@ -55,5 +31,29 @@
             :preconds '(have-money)
             :add-list '(shop-has-money)
             :del-list '(have-money))))
+
+(defun executing-p (x)
+  "Is x of the form: (executing ...)?"
+  (starts-with x 'executing))
+
+(defun starts-with (list x)
+  "Is this a list whose first element is x?"
+  (and (consp list) (eql (first list) x)))
+
+;; Since the elements of the add-list are to be added to the state
+;; upon execution of the operator, adding a (executing ...) form to
+;; the add-list make this (executing ...) form appear on the list
+;; which represents the state after the application of the operator.
+(defun convert-op (op)
+  "Make op conform to the (EXECUTING op) convention."
+  (unless (some #'executing-p (op-add-list op))
+    (push (list 'executing (op-action op)) (op-add-list op)))
+  op)
+
+(defun op (action &key preconds add-list del-list)
+  "Make a new operator that obeys the (EXECUTING op) convention."
+  (convert-op
+   (make-op :action action :preconds preconds :add-list add-list
+            :del-list del-list)))
 
 (mapc #'convert-op *school-ops*)
