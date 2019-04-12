6fc3354073908e34d98a8ec2a13e0c5965fcb59c
Grigory Bordyugov <gregbg@gmail.com>
2019-04-12 09:43:16 +0200

    Chapter 4: moving blocks is working

--- chapter04/gps-v2.lisp
+++ chapter04/gps-v2.lisp
@@ -271,3 +271,43 @@
 (find-path 1 1)
 
 (equal (find-path 1 25) (reverse (find-path 25 1)))
+
+;;
+;; Chapter 4.14 The Blocks World Domain
+;;
+
+(defun make-block-ops (blocks)
+  (let ((ops nil))
+    (dolist (a blocks)
+      (dolist (b blocks)
+        (unless (equal a b)
+          (dolist (c blocks)
+            (unless (or (equal c a) (equal c b))
+              (push (move-op a b c) ops)))
+          (push (move-op a 'table b) ops)
+          (push (move-op a b 'table) ops))))
+    ops))
+
+(defun move-op (a b c)
+  "Make an operator to move A from B to C."
+  (op `(move ,a from ,b to ,c)
+      :preconds `((space on ,a) (space on ,c) (,a on ,b))
+      :add-list (move-ons a b c)
+      :del-list (move-ons a c b)))
+
+(defun move-ons (a b c)
+  (if (eq b 'table)
+      `((,a on ,c))
+      `((,a on ,c) (space on ,b))))
+
+(use (make-block-ops '(a b)))
+
+(gps '((a on table) (b on table) (space on a) (space on b) (space on table))
+     '((a on b) (b on table)))
+
+(debug-norvig :gps)
+
+(gps '((a on b) (b on table) (space on a) (space on table))
+     '((b on a)))
+
+(undebug-norvig)
