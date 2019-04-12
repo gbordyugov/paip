a3deb98a9eaaa2f9d17550346fad807569f4c39b
Grigory Bordyugov <gregbg@gmail.com>
2019-04-12 09:27:59 +0200

    Chapter 4 maze code working

--- chapter04/gps-v2.lisp
+++ chapter04/gps-v2.lisp
@@ -219,3 +219,33 @@
 
 (gps '(at-door on-floor has-ball hungry chair-at-door)
      '(not-hungry))
+
+;;
+;; Chapter 4.13 The Maze Searching Domain
+;;
+
+(defun make-maze-ops (pair)
+  "Make maze ops in both directions."
+  (list (make-maze-op (first pair) (second pair))
+        (make-maze-op (second pair) (first pair))))
+
+(defun make-maze-op (here there)
+  "Make an operator to move between two places."
+  (op `(move from ,here to ,there)
+      :preconds `((at ,here))
+      :add-list `((at ,there))
+      :del-list `((at ,here))))
+
+(defun mappend (fn the-list)
+  "Apply fn to each element of list and append the results."
+  (apply #'append (mapcar fn the-list)))
+
+(defparameter *maze-ops*
+  (mappend #'make-maze-ops
+           '((1 2) (2 3) (3 4) (4 9) (9 14) (9 8) (8 7) (7 12) (12 13)
+             (12 11) (11 6) (11 16) (16 17) (17 22) (21 22) (22 23)
+             (28 18) (23 24) (24 19) (19 20) (20 15) (15 10) (10 5) (20 25))))
+
+(use *maze-ops*)
+
+(gps '((at 1)) '((at 25)))
