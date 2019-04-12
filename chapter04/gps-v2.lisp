02fdbb85ecf03bf50af4876b31250348f31c9707
Grigory Bordyugov <gregbg@gmail.com>
2019-04-12 09:20:37 +0200

    Chapter 4.12 done (Monkey and Bananas)

--- chapter04/gps-v2.lisp
+++ chapter04/gps-v2.lisp
@@ -178,3 +178,44 @@
 
 ;; A trivial one.
 (gps '(son-at-home) '(son-at-home))
+
+
+;;
+;; Chapter 4.12 Monkey and Bananas
+;;
+
+(defparameter *banana-ops*
+  (list
+   (op 'climb-on-chair
+       :preconds '(chair-at-middle-room at-middle-room on-floor)
+       :add-list '(at-bananas on-chair)
+       :del-list '(at-middle-room on-floor))
+   (op 'push-chair-from-door-to-middle-room
+       :preconds '(chair-at-door at-door)
+       :add-list '(chair-at-middle-room at-middle-room)
+       :del-list '(chair-at-door at-door))
+   (op 'walk-from-door-to-middle-room
+       :preconds '(at-door on-floor)
+       :add-list '(at-middle-room)
+       :del-list '(at-door))
+   (op 'grasp-bananas
+       :preconds '(at-bananas empty-handed)
+       :add-list '(has-bananas)
+       :del-list '(empty-handed))
+   (op 'drop-ball
+       :preconds '(has-ball)
+       :add-list '(empty-handed)
+       :del-list '(has-ball))
+   (op 'eat-bananas
+       :preconds '(has-bananas)
+       :add-list '(empty-handed not-hungry)
+       :del-list '(has-bananas hungry))))
+
+(use *banana-ops*)
+
+(debug-norvig :gps)
+
+(undebug-norvig)
+
+(gps '(at-door on-floor has-ball hungry chair-at-door)
+     '(not-hungry))
