782b0b337edacf19f17b727f7e2bbb2c81c2b899
Grigory Bordyugov <gregbg@gmail.com>
2019-05-06 19:04:39 +0200

    Exercise 4.4: added some documentation

--- chapter04/exercise-4.4.lisp
+++ chapter04/exercise-4.4.lisp
@@ -2,6 +2,16 @@
 ;; Exercise 4.4
 ;;
 
+;;
+;; The Not Looking after You Don't Leap Problem. Write a program that
+;; keeps track of the remaining goals so that it does not get stuck
+;; considering only one possible operation when others will eventually
+;; lead to the goal. Hint: have `achieve` take an extra argument
+;; indicating the goals that remain to be achieved after the current
+;; goal is achieved. `achieve` should succeed only if it can achieve
+;; the current goal and also `achieve-all` the remaining goals.
+;;
+
 (defstruct op "An operation"
            (action nil)
            (preconds nil)
