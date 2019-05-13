02081a8102febb021d396e6bd78693f7f6e0947c
Grigory Bordyugov <gregbg@gmail.com>
2019-05-13 10:03:45 +0200

    small refactoring

--- chapter04/exercise-4.4.lisp
+++ chapter04/exercise-4.4.lisp
@@ -98,12 +98,11 @@
 (defun achieve-each (state goals goal-stack)
   "Achieve each goal, and make sure they still hold at the end."
   (let ((current-state state))
-    (if (and (every #'(lambda (g)
-                        (setf current-state
-                              (achieve current-state g goal-stack)))
-                    goals)
-             (subsetp goals current-state :test #'equal))
-        current-state)))
+    (labels ((local-achieve (g)
+               (setf current-state (achieve current-state g goal-stack))))
+      (if (and (every #'local-achieve goals)
+               (subsetp goals current-state :test #'equal))
+          current-state))))
 
 (defun orderings (l)
   "For a list with one element, returns a list, consisting of the
