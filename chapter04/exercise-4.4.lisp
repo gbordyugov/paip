6b113dd9d4ed8b18871ed1a69e8d47f13898ba8f
Grigory Bordyugov <gregbg@gmail.com>
2019-05-27 10:04:10 +0200

    Exercise 4.4: rename achieve -> achieve-main

--- chapter04/exercise-4.4.lisp
+++ chapter04/exercise-4.4.lisp
@@ -155,13 +155,13 @@
    they still hold at the end."
   (let ((current-state state))
     (labels ((local-achieve (g)
-               (setf current-state (achieve current-state g goal-stack '()))))
+               (setf current-state (achieve-main current-state g goal-stack '()))))
       (if (and (every #'local-achieve goals)
                ;; make sure that all target goals still hold
                (subsetp goals current-state :test #'equal))
           current-state))))
 
-(defun achieve (state goal goal-stack remaining-goals)
+(defun achieve-main (state goal goal-stack remaining-goals)
   "A goal is achieved if it already holds, or if there is an
    appropriate op for it that is applicable."
   (dbg-indent :gps (length goal-stack) "Goal: ~a" goal)
