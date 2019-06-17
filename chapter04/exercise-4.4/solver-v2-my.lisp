d82d655ca04bd5fd02a6c1ab7840a53bf23c2f8f
Grigory Bordyugov <gregbg@gmail.com>
2019-06-17 11:11:52 +0200

    Exercise 4.4: start rewriting solver from scratch

--- chapter04/exercise-4.4/solver-v2-my.lisp
+++ chapter04/exercise-4.4/solver-v2-my.lisp
@@ -3,10 +3,13 @@
 ;; achieve-all -> achieve -> apply-op
 ;;
 
-(defun achieve-all (state goal goal-stack)
-  t)
+(defun achieve-all (state goals goal-stack)
+  (labels ((achieve-state (from-state to-state)
+             (when from-state
+               (achieve from-state to-state goal-stack))))
+    t))
 
-(defun achieve (state goal goal-stack)
+(defun achieve (state goals goal-stack)
   t)
 
 (defun apply-op (state op goal-stack)
