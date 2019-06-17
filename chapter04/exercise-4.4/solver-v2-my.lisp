9bad43a6700b84b4fd0e5fd97948c3b8307fb10a
Grigory Bordyugov <gregbg@gmail.com>
2019-06-17 10:13:18 +0200

    Exercise 4.4: start re-writing solver-v2 from scratch

---
+++ chapter04/exercise-4.4/solver-v2-my.lisp
@@ -0,0 +1,16 @@
+;;
+;; The chain of calls
+;; achieve-all -> achieve -> apply-op
+;;
+
+(defun achieve-all (state goal goal-stack)
+  t)
+
+(defun achieve (state goal goal-stack)
+  t)
+
+(defun apply-op (state op goal-stack)
+  t)
+
+(defun gps (start-state end-state)
+  t)
