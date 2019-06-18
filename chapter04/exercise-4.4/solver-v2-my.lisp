23b14f920f54264082dfe1ca08b85259b9b5fc5b
Grigory Bordyugov <gregbg@gmail.com>
2019-06-18 09:42:11 +0200

    Exercise 4.4: my implementation of achieve-all

--- chapter04/exercise-4.4/solver-v2-my.lisp
+++ chapter04/exercise-4.4/solver-v2-my.lisp
@@ -4,10 +4,15 @@
 ;;
 
 (defun achieve-all (state goals goal-stack)
+  "Achieve all goals in sequence, using the state after achieving the
+   previous goal as the starting state for achieving the next goal.
+   Check if the end state is a superset of goals."
   (labels ((achieve-state (from-state to-state)
              (when from-state
                (achieve from-state to-state goal-stack))))
-    t))
+    (let* ((end-state (reduce #'achieve-state goals :initial-value state))
+           (all-goals-reached-p (subsetp goals end-state :test #'equal)))
+      (and end-state and all-goals-reached-p))))
 
 (defun achieve (state goals goal-stack)
   t)
