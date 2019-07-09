03afe13138c51fb4a2d0a2da79fd6881921d8ff1
Grigory Bordyugov <gregbg@gmail.com>
2019-07-09 11:52:33 +0200

    Exercise 4.4: re-groupe code

--- chapter04/exercise-4.4/exercise-4.4.lisp
+++ chapter04/exercise-4.4/exercise-4.4.lisp
@@ -33,35 +33,38 @@
 ;; valid solution: Driving son to school ourselves.
 ;;
 
-(load "ops.lisp")
-
-(load "school-ops.lisp")
-
-(load "debug.lisp")
-
-(load "solver-v2-my.lisp")
-
-(use *school-ops*)
-
 ;;
 ;; The standard problem.
 ;;
-(GPS '(son-at-home car-needs-battery have-money have-phone-book)
-     '(son-at-school))
+(defun run-normal ()
+  (load "ops.lisp")
+  (load "school-ops.lisp")
+  (load "debug.lisp")
+  (load "solver-v2-my.lisp")
+  (use *school-ops*)
+  (GPS '(son-at-home car-needs-battery have-money have-phone-book)
+       '(son-at-school)))
 
-(use (push (op 'taxi-son-to-school
-               :preconds '(son-at-home have-money)
-               :add-list '(son-at-school)
-               :del-list '(son-at-home have-money))
-           *school-ops*))
+(run-normal)
 
-(debug-norvig :gps)
+(defun run-problematic ()
+  (load "ops.lisp")
+  (load "school-ops.lisp")
+  (load "debug.lisp")
+  (load "solver-v2-my.lisp")
+  (use (push (op 'taxi-son-to-school
+                 :preconds '(son-at-home have-money)
+                 :add-list '(son-at-school)
+                 :del-list '(son-at-home have-money))
+             *school-ops*))
+  (debug-norvig :gps)
+  ;;
+  ;; This one doesn't work because of the not looking after you don't
+  ;; leap problem. The actual solution would be to drive son to school
+  ;; ourselves, but the solver gets stuck by driving son to school with
+  ;; taxi and then trying to fulfill `have-money` goal.
+  ;;
+  (GPS '(son-at-home have-money car-works)
+       '(son-at-school have-money)))
 
-;;
-;; This one doesn't work because of the not looking after you don't
-;; leap problem. The actual solution would be to drive son to school
-;; ourselves, but the solver gets stuck by driving son to school with
-;; taxi and then trying to fulfill `have-money` goal.
-;;
-(GPS '(son-at-home have-money car-works)
-     '(son-at-school have-money))
+(run-problematic)
