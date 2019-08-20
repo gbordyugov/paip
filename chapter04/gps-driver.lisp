658ac772c6d472d0fb06a932e292a053a556754b
Grigory Bordyugov <gregbg@gmail.com>
2019-08-20 09:31:09 +0200

    Chapter 4 take 2: the clobbering sibling goal problem

--- chapter04/gps-driver.lisp
+++ chapter04/gps-driver.lisp
@@ -11,3 +11,29 @@
 (gps '(son-at-home car-works)
      '(son-at-school)
      *school-ops*)
+
+;;
+;; 4.7 The Clobbering Sibling Goal Problem
+;;
+
+;;
+;; This case works as expected by driving son to school.
+;;
+(gps '(son-at-home have-money car-works)
+     '(have-money son-at-school)
+     *school-ops*)
+
+;;
+;; But in this case, GPS incorrectly reports success despite it having
+;; spent money on the battery.
+;;
+(gps '(son-at-home car-needs-battery have-money have-phone-book)
+     '(have-money son-at-school)
+     *school-ops*)
+;;
+;; ... the bug being that the expression (every #'achieve goals) tries
+;; to achieve goals in sequence, which means that achieving achieving
+;; a subsequent goal can undo a previously achieved goal. This means
+;; that achieving (have-money son-at-school) means "first achieve
+;; have-money and then achieve son-at-school".
+;;
