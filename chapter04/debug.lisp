71b5b433394169e519598de7a31c01b2c5e4d6f2
Grigory Bordyugov <gregbg@gmail.com>
2019-08-23 14:13:26 +0200

    Chapter 4 take 2: add debug.lisp

---
+++ chapter04/debug.lisp
@@ -0,0 +1,17 @@
+(defvar *dbg-ids* nil "Indentifiers used by dbg")
+
+(defun norvig-dbg (id format-string &rest args)
+  "Print debugging info if (DEBUG ID) has been specified."
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (apply #'format *debug-io* format-string args)))
+
+(defun norvig-debug (&rest ids)
+  "Start dbg output on the given ids."
+  (setf *dbg-ids* (union ids *dbg-ids*)))
+
+(defun norvig-undebug (&rest ids)
+  "Stop dbg on the ids. With no ids, stop dbg altogether."
+  (setf *dbg-ids* (if (null ids)
+                      nil
+                      (set-difference *dbg-ids* ids))))
