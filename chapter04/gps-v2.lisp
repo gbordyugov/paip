93db8c6c93c419d36ed7af519ac17001e5db3a23
Grigory Bordyugov <gregbg@gmail.com>
2019-08-26 10:13:19 +0200

    Chapter 4 take 2: started working on GPS v2

---
+++ chapter04/gps-v2.lisp
@@ -0,0 +1,14 @@
+(defvar *ops* nil "A list of available operators.")
+
+(defstruct op
+  "An operation"
+  (action nil)
+  (preconds nil)
+  (add-list nil)
+  (del-list nil))
+
+(load "utils.lisp")
+
+(defun GPS (state goals &optional (*ops* *ops*))
+  "General Problem Solver: from state, achieve goals using *ops*."
+  (remove-if #'atom (achieve-all (cons '(start) state) goals nil)))
