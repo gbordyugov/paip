d1b93967accdaf90fb1a405e6ea4c41d30ed96c8
Grigory Bordyugov <gregbg@gmail.com>
2019-08-26 10:15:15 +0200

    Chapter 4 task 2: add docstring

--- chapter04/gps-v2.lisp
+++ chapter04/gps-v2.lisp
@@ -11,4 +11,7 @@
 
 (defun GPS (state goals &optional (*ops* *ops*))
   "General Problem Solver: from state, achieve goals using *ops*."
+  ;; Note that we extend the initial condition by appending a '(START)
+  ;; list to it, so starting even with an empty list will produce
+  ;; meaningful initial condition.
   (remove-if #'atom (achieve-all (cons '(start) state) goals nil)))
