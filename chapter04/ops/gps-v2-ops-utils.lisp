14739bb92fd646c33c4437d4a2669d2d53dc29b6
Grigory Bordyugov <gregbg@gmail.com>
2019-08-26 09:18:32 +0200

    Chapter 4 take 2: GPS2 op utils

---
+++ chapter04/ops/gps-v2-ops-utils.lisp
@@ -0,0 +1,20 @@
+(defun executing-p (x)
+  "Is x of the form: (executing ...)?"
+  (starts-with x 'executing))
+
+(defun starts-with (list x)
+  "Is this a list whose first element ix x?"
+  (and (consp list) (eql (first list) x)))
+
+(defun convert-op (op)
+  "Make op conform to the (EXECUTING op) convention."
+  (unless (some #'executing-p (op-add-list op))
+    (push (list 'executing (op-action op))
+          (op-add-list op))
+    op))
+
+(defun op (action &key preconds add-list del-list)
+  "Make a new operator that obeys the (EXECUTING op) convention."
+  (convert-op
+   (make-op :action action :preconds preconds
+            :add-list add-list :del-list del-list)))
