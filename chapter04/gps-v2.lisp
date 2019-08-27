7e178707a07d91e7776f50be5b8b8c326169b1e7
Grigory Bordyugov <gregbg@gmail.com>
2019-08-27 09:40:19 +0200

    Chapter 4 take 2: add docstring

--- chapter04/gps-v2.lisp
+++ chapter04/gps-v2.lisp
@@ -14,4 +14,7 @@
   ;; Note that we extend the initial condition by appending a '(START)
   ;; list to it, so starting even with an empty list will produce
   ;; meaningful initial condition.
+  ;; The purpose of it to make '() / nil less ambiguous: it now
+  ;; represents a failure rather than an empty set of conditions. In
+  ;; other words, a state can never become an empty list.
   (remove-if #'atom (achieve-all (cons '(start) state) goals nil)))
