4f1e2a009ce1b999dad0c8024b24b952fbfa9332
Grigory Bordyugov <gregbg@gmail.com>
2019-09-04 11:09:21 +0200

    Chapter 4 take 2: making block solver get stuck

--- chapter04/blocks-driver.lisp
+++ chapter04/blocks-driver.lisp
@@ -6,3 +6,27 @@
 (gps '((a on table) (b on table) (space on a) (space on b)
        (space on table))
      '((a on b) (b on table)))
+
+(norvig-debug :gps)
+
+(gps '((a on b) (b on table) (space on a) (space on table))
+     '((b on a)))
+
+(norvig-undebug :gps)
+
+(progn
+  (load "gps-v2-maze.lisp")
+  (load "ops/block-ops.lisp")
+  (use (make-block-ops '(a b c)))
+  ;; This combination of goals produces a solution.
+  (gps '((a on b) (b on c) (c on table) (space on a) (space on table))
+       '((b on a) (c on b))))
+
+(progn
+  (load "gps-v2-maze.lisp")
+  (load "ops/block-ops.lisp")
+  (use (make-block-ops '(a b c)))
+  ;; Whereas a permutation of the goal state make the solver get
+  ;; stuck.
+  (gps '((a on b) (b on c) (c on table) (space on a) (space on table))
+       '((c on b) (b on a))))
