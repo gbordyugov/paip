19437afc54a945bdb51bfa5fa4c6dcd493c0b635
Grigory Bordyugov <gregbg@gmail.com>
2019-09-23 09:41:09 +0200

    Chapter 4 take 2: working on Exercise 4.2

--- chapter04/expercise4-2.lisp
+++ chapter04/expercise4-2.lisp
@@ -4,6 +4,8 @@
 (defun permutations (items)
   (if (< (length items) 2)
       items
-      '()))
+      ;; I don't really know how to best proceed here.
+      (let ((perms (permutations (cdr items))))
+        (mapcar #'(lambda (x) (x) items)))))
 
 (permutations '(1 2))
