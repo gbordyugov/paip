a297962f264956bd6600d992b7ab43ef7c800d0e
Grigory Bordyugov <gregbg@gmail.com>
2019-09-24 09:47:56 +0200

    Chapter 4 take 2: working on Exercise 4.2

--- chapter04/expercise4-2.lisp
+++ chapter04/expercise4-2.lisp
@@ -1,11 +1,22 @@
 ;;
 ;; Exercise 4.2
 ;;
+
+(defun mappend (fn the-list)
+  "Apply fn to each element of list and append the results."
+  (apply #'append (mapcar fn the-list)))
+
+(defun insert-at-each-position (x items)
+  ;; Return list of lists consisting of inserting x into items at all
+  ;; possible positions.
+  '())
+
 (defun permutations (items)
+  ;; Return all permutations of items.
   (if (< (length items) 2)
       items
       ;; I don't really know how to best proceed here.
-      (let ((perms (permutations (cdr items))))
+      (let ((rest-perms (permutations (cdr items))))
         (mapcar #'(lambda (x) (x) items)))))
 
 (permutations '(1 2))
