a00d74f2a688b300ba8465c7781fd7b10d6b6fe2
Grigory Bordyugov <gregbg@gmail.com>
2019-10-22 22:48:51 +0100

    Chapter 4 take 2: another Python version of permutations

--- chapter04/exercise4-2.py
+++ chapter04/exercise4-2.py
@@ -10,3 +10,11 @@ def permutations(bag):
             rest_perms = permutations(rest_items)
             return [[item] + p for p in rest_perms]
         return mapcan(f, bag)
+
+def permutations_1(bag):
+    bag = list(bag)
+    if len(bag) == 0:
+        return [[]]
+    else:
+        return [x for item in bag
+                  for x in [[item] + p for p in permutations_1(e for e in bag if e != item)]]
