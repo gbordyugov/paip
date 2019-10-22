e6ebdf3d90001b9184c7ed4850f949fe1c667a5d
Grigory Bordyugov <gregbg@gmail.com>
2019-10-22 23:12:34 +0100

    Chapter 4 take 2: started working on Exercise 4.3

---
+++ chapter04/exercise4-3.lisp
@@ -0,0 +1,16 @@
+(load "ops/op.lisp")
+
+(defparameter *ex-4-3-ops*
+  (list
+   (op 'buy-cake
+       :preconds '(have-money)
+       :add-list '(have-cake)
+       :del-list '(have-money))
+   (op 'eat-ice-cream
+       :preconds '(have-ice-cream)
+       :add-list '()
+       :del-list '(have-ice-cream))
+   (op 'eat-cake
+       :preconds '(have-cake)
+       :add-list '()
+       :del-list '(have-cake))))
