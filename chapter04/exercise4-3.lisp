304cffb7c573bb9f258860db022d864d441cfa48
Grigory Bordyugov <gregbg@gmail.com>
2019-10-23 20:53:40 +0100

    Chapter 4 take 2 exercise 4.3: make a big progn

--- chapter04/exercise4-3.lisp
+++ chapter04/exercise4-3.lisp
@@ -1,24 +1,25 @@
-(load "gps-v2-maze.lisp")
-(load "utils.lisp")
-(load "ops/op.lisp")
+(progn
+  (load "gps-v2-maze.lisp")
+  (load "utils.lisp")
+  (load "ops/op.lisp")
 
-(defparameter *ex-4-3-ops*
-  (list
-   (op 'buy-cake
-       :preconds '(have-money)
-       :add-list '(have-cake)
-       :del-list '(have-money))
-   (op 'eat-ice-cream
-       :preconds '(have-ice-cream)
-       :add-list '()
-       :del-list '(have-ice-cream))
-   (op 'eat-cake
-       :preconds '(have-cake)
-       :add-list '(have-ice-cream)
-       :del-list '(have-cake))))
+  (defparameter *ex-4-3-ops*
+    (list
+     (op 'buy-cake
+         :preconds '(have-money)
+         :add-list '(have-cake)
+         :del-list '(have-money))
+     (op 'eat-ice-cream
+         :preconds '(have-ice-cream)
+         :add-list '()
+         :del-list '(have-ice-cream))
+     (op 'eat-cake
+         :preconds '(have-cake)
+         :add-list '(have-ice-cream)
+         :del-list '(have-cake))))
 
-(mapc #'convert-op *ex-4-3-ops*)
+  (mapc #'convert-op *ex-4-3-ops*)
 
-(gps '(have-money)
-     '(have-cake)
-     *ex-4-3-ops*)
+  (gps '(have-money)
+       '(have-cake)
+       *ex-4-3-ops*))
