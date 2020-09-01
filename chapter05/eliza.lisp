35b6afd0e899de3674787f2c36329cf320bff4fb
Grigory Bordyugov <gregbg@gmail.com>
2020-09-01 09:43:12 +0200

    working on pattern matching from Chapter 5

--- chapter05/eliza.lisp
+++ chapter05/eliza.lisp
@@ -43,3 +43,58 @@
 ;; My answer is "no" as the solution with every would output T when
 ;; trying to match pattern and input of different lengths, for example
 ;; '(a b c) with '(a b)
+
+;;
+;; There is a tradition in Lisp-based AI programs to have variables by
+;; symbols that start with the question mark character.
+
+(symbol-name 'abc)
+
+(defun variable-p (x)
+  "Is ix a variable, i.e., a symbol beginning with '?' ?"
+  (and (symbolp x)
+       (equal (char (symbol-name x) 0)
+              #\?)))
+
+;; should evaluate to T
+(pat-match '(I need a ?X) '(I need a vacation))
+
+;; should evaluate to NIL
+(pat-match '(I need a ?X) '(I really need a vacation))
+
+(sublis '((?X . vacation))
+        '(what would it mean to you if you got a ?X ?))
+
+(defun pat-match (pattern input)
+  "Does pattern match input? WARNING: buggy version."
+  (if (variable-p pattern)
+      (list (cons pattern input))
+      (if (or (atom pattern) (atom input))
+          (eql pattern input)
+          (append (pat-match (first pattern) (first input))
+                  (pat-match (rest pattern) (rest input))))))
+
+;;
+;; The above implementation of pat-match has the following problems:
+;;
+;; 1) (eql pattern input) may return T, which is not a list, so a
+;;    consequitive append will complain.
+;;
+;; 2) the same tist might return NIL, which would indicate failure,
+;;    but it will just be treated as a list, and will be appended to
+;;    the rest of answer.
+;;
+;; 3) we cannot distinguish between the case where the match fails -
+;;    and returns NIL -- versus the case where everything matches, but
+;;    there are no variable, so it returns the null a-list (this is
+;;    the semipredicate problem already discussed in Chapter 4).
+;;
+;; 4) we want the bindings of variables to agree -- if ?X is used
+;;    twice in the patter, we don't want it to match two different
+;;    values in the input.
+;;
+;; 5) it is inefficient for pat-match to check both the first and the
+;;    rest of lists, even when the corresponding first parts fail to
+;;    match.
+;;
+;; A total of five bugs in a seven-line function.
